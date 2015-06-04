f2f723f9a8758fc2097b7c43c94a13fa6bf647c2
cleggatt <cleggatt@users.noreply.github.com>
2015-06-04 13:26:16 +1000

    Remove some percentage of dead ends.

--- generate.js
+++ generate.js
@@ -1,6 +1,11 @@
 angular.module('generateApp', [])
     .controller('MazeController', ['$scope', '$interval', function($scope, $interval) {
 
+        var Point = function Point(x, y) {
+            this.x = x;
+            this.y = y;
+        };
+
         var Cell = function Cell() {
 
             this.walls = [true, true, true, true];
@@ -27,10 +32,46 @@ angular.module('generateApp', [])
                 }
             }
 
-            this.x = Math.floor(Math.random() * width);
-            this.y = Math.floor(Math.random() * height);
-            this.cells[this.x][this.y].visited = true;
-            this.history = [];
+            this.setActivePosition = function(x, y, state) {
+                this.x = x;
+                this.y = y;
+                // TODO Use acc.reversing
+                this.state = state;
+            }
+        };
+
+        var MazeBuilder = function MazeBuilder(grid) {
+
+            this.grid = grid;
+
+            this.init = function(acc) {
+
+                acc.reversing = false;
+                acc.history = [];
+                acc.deadEnds = [];
+
+                // TODO does this mean we're skipping an animation frame?
+                this.x = Math.floor(Math.random() * this.grid.width);
+                this.y = Math.floor(Math.random() * this.grid.height);
+                this.grid.cells[this.x][this.y].visited = true;
+
+                return true;
+            };
+
+            this.step = function(acc) {
+
+                var newPosition = this.findValidPosition();
+                if (newPosition) {
+                    this.moveForwardTo(acc, newPosition[0], newPosition[1], newPosition[2]);
+                } else if (!this.backUp(acc)) {
+                    console.log("All moves exhausted!");
+                    acc.reversing = false;
+                    return false;
+                }
+
+                this.grid.setActivePosition(this.x, this.y, acc.reversing);
+                return true;
+            };
 
             this.findValidPosition = function() {
 
@@ -54,9 +95,9 @@ angular.module('generateApp', [])
                     }
 
                     console.log("Checking (" + newX + "," + newY + ")...");
-                    if (newX < 0 || newX >= this.width || newY < 0 || newY >= this.height) {
+                    if (newX < 0 || newX >= this.grid.width || newY < 0 || newY >= this.grid.height) {
                         console.log("Cannot move - off grid");
-                    } else if (this.cells[newX][newY].visited) {
+                    } else if (this.grid.cells[newX][newY].visited) {
                         console.log("Cannot move - visited");
                     } else {
                         return [newX, newY, direction];
@@ -70,35 +111,193 @@ angular.module('generateApp', [])
                 return undefined;
             };
 
-            this.step = function() {
+            this.moveForwardTo = function(acc, newX, newY, direction) {
+                acc.reversing = false;
 
-                var newPosition = this.findValidPosition();
+                this.grid.cells[this.x][this.y].walls[direction] = false;
+                acc.history.push([this.x, this.y]);
 
-                if (newPosition) {
-                    this.cells[this.x][this.y].walls[newPosition[2]] = false;
-                    this.history.push([this.x, this.y]);
-
-                    this.x = newPosition[0];
-                    this.y = newPosition[1];
-                    this.cells[this.x][this.y].walls[this.convertDirection(newPosition[2])] = false;
-                    this.cells[this.x][this.y].visited = true;
-                    console.log("Moved to (" + newPosition[0] + "," + newPosition[1] + ")");
-
-                    return true;
-                } else if (this.history.length == 0) {
-                    console.log("All moves exhausted!");
-                    return false;
-                } else {
-                    var last = this.history.pop();
-                    console.log("Returning to (" + last[0] + "," + last[1] + ")...");
-                    this.x = last[0];
-                    this.y = last[1];
-                    return true;
-                }
+                this.x = newX;
+                this.y = newY;
+                this.grid.cells[this.x][this.y].walls[this.convertDirection(direction)] = false;
+                this.grid.cells[this.x][this.y].visited = true;
+                console.log("Moved to (" + this.x + "," + this.y + ")");
             };
 
             this.convertDirection = function(direction) {
                 return (direction + 2) % 4;
+            };
+
+            this.backUp = function(acc) {
+                if (acc.history.length == 0) {
+                    return false;
+                }
+
+                if (acc.reversing == false) {
+                    acc.deadEnds.push(new Point(this.x, this.y));
+                }
+                acc.reversing = true;
+                var last = acc.history.pop();
+                console.log("Returning to (" + last[0] + "," + last[1] + ")...");
+                this.x = last[0];
+                this.y = last[1];
+
+                return true;
+            };
+        };
+
+        var DeadEndFiller = function DeadEndFiller(grid, deadendPercentage) {
+
+            this.grid = grid;
+            this.deadendPercentage = deadendPercentage;
+
+            this.init = function(acc) {
+                this.deadEnds = acc.deadEnds.slice(0);
+
+                this.deadEndsToKeep = Math.ceil(this.deadEnds.length * (this.deadendPercentage / 100));
+
+                this.position = this.pickDeadEnd();
+
+                return (this.position != null);
+            };
+
+            this.step = function(acc) {
+
+                var oldPosition = this.position;
+
+                this.position = this.findValidPosition();
+
+                if (this.position) {
+                    this.fillCell(oldPosition);
+                } else {
+                    this.position = this.pickDeadEnd();
+                    if (this.position) {
+                        console.log("Jumping to (" + this.position.x + "," + this.position.y + ")");
+                    }
+                }
+
+                if (this.position) {
+                    this.grid.setActivePosition(this.position.x, this.position.y, true);
+                }
+
+                return (this.position != null);
+            };
+
+            this.pickDeadEnd = function() {
+
+                if (this.deadEnds.length == 0 | this.deadEnds.length == this.deadEndsToKeep) {
+                    console.log("No remaining dead ends...");
+                    return null;
+                }
+
+                var idx = Math.floor(Math.random() * this.deadEnds.length);
+                var deadEnd = this.deadEnds.splice(idx, 1)[0];
+                return deadEnd;
+            };
+
+            this.findValidPosition = function() {
+
+                console.log("Finding valid position...");
+
+                var direction = this.findSingleExit(this.grid.cells[this.position.x][this.position.y].walls);
+
+                if (direction != -1) {
+                    var newX = this.position.x;
+                    var newY = this.position.y;
+
+                    if (direction == 0) {
+                        newY--;
+                    } else if (direction == 1) {
+                        newX++;
+                    } else if (direction == 2) {
+                        newY++;
+                    } else { // (direction == 3)
+                        newX--;
+                    }
+
+                    var newPosition = new Point(newX, newY);
+
+                    console.log("Moving to (" + newPosition.x + "," + newPosition.y + ")");
+
+                    return newPosition;
+                } else {
+                    console.log("No valid positions!");
+                    return null;
+                }
+            };
+
+            this.findSingleExit = function(walls) {
+
+                var lastExit = -1;
+                for (var direction = 0; direction < 4; direction++) {
+                    if (!walls[direction]) {
+                        if (lastExit == -1) {
+                            lastExit = direction;
+                        } else {
+                            console.log("Multiple exits!");
+                            return -1;
+                        }
+                    }
+                }
+
+                if (lastExit == -1) {
+                    console.log("No exits!");
+                    return -1;
+                }
+
+                return lastExit;
+            };
+
+            this.fillCell = function(position) {
+                // TODO Need to fill our open wall AND our neighbours open wall
+                console.log("Filling (" + position.x + "," + position.y + ")");
+
+                var walls = grid.cells[position.x][position.y].walls;
+
+                if (!walls[0]) {
+                    this.grid.cells[position.x][position.y - 1].walls[this.convertDirection(0)] = true;
+                }
+                if (!walls[1]) {
+                    this.grid.cells[position.x + 1][position.y].walls[this.convertDirection(1)] = true;
+                }
+                if (!walls[2]) {
+                    this.grid.cells[position.x][position.y + 1].walls[this.convertDirection(2)] = true;
+                }
+                if (!walls[3]) {
+                    this.grid.cells[position.x - 1][position.y].walls[this.convertDirection(3)] = true;
+                }
+
+                this.grid.cells[position.x][position.y].reset();
+            };
+
+            this.convertDirection = function(direction) {
+                return (direction + 2) % 4;
+            };
+        };
+
+        var MapGenerator = function MapGenerator(builders) {
+
+            this.acc = {};
+
+            var allBuilders = builders.slice(0);
+            var currentBuilder = allBuilders.shift();
+
+            // TODO Handle false from this
+            currentBuilder.init(this.acc);
+
+            this.step = function() {
+
+               if (currentBuilder.step(this.acc)) {
+                   return true;
+               }
+
+               if (allBuilders.length == 0) {
+                   return false;
+               }
+
+               currentBuilder = allBuilders.shift();
+
+               return currentBuilder.init(this.acc);
             }
         };
 
@@ -154,7 +353,12 @@ angular.module('generateApp', [])
                         }
                     }
 
-                    ctx.fillStyle = '#FF0000';
+                    if (this.grid.state) {
+                        ctx.fillStyle = '#FF0000';
+                    } else {
+                        ctx.fillStyle = '#00FF00';
+                    }
+
                     ctx.fillRect(this.grid.x * this.cellSize, this.grid.y * this.cellSize, this.cellSize, this.cellSize);
                 }
 
@@ -187,7 +391,7 @@ angular.module('generateApp', [])
             };
         };
 
-        $scope.params = {width: 10, height: 10, size: 50, speed : 50};
+        $scope.params = {width: 5, height: 5, size: 50, deadEnds: 100, speed : 50};
 
         $scope.gridCanvas = new GridCanvas(document.getElementById("map"));
 
@@ -200,14 +404,17 @@ angular.module('generateApp', [])
 
             $scope.grid = new Grid($scope.params.width, $scope.params.height);
 
+            $scope.mapGenerator = new MapGenerator([ new MazeBuilder($scope.grid),
+                new DeadEndFiller($scope.grid, parseInt($scope.params.deadEnds)) ]);
+
             $scope.gridCanvas.setCellSize($scope.params.size);
             $scope.gridCanvas.setGrid($scope.grid);
             $scope.gridCanvas.activate();
 
             stop = $interval(function() {
-                var movesRemain = $scope.grid.step();
+                var stepsRemain = $scope.mapGenerator.step();
 
-                if (!movesRemain) {
+                if (!stepsRemain) {
                     $scope.gridCanvas.deactivate();
                     $scope.stopGeneration();
                 }
