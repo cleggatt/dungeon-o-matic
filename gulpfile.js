13ecc23c0eb18d6de8e044953d1f85f1090cd1c4
cleggatt <cleggatt@users.noreply.github.com>
2015-07-12 12:13:15 +1000

    Add "clean" Gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var gutil = require("gulp-util");
+
+var del = require('del');
 var istanbul = require('gulp-istanbul');
 var jasmine = require('gulp-jasmine');
 var jshint = require('gulp-jshint');
@@ -9,6 +11,10 @@ var WebpackDevServer = require("webpack-dev-server");
 var webpackConfig = require("./webpack.config.js");
 
 gulp.task('default', ['webpack'], function () {
+});
+
+gulp.task('clean', function (callback) {
+    del(['build/'], callback);
 });
 
 gulp.task('lint', function() {
@@ -34,7 +40,7 @@ gulp.task('test-cover', ['lint'], function (callback) {
         });
 });
 
-gulp.task("webpack", ['test-cover'], function(callback) {
+gulp.task("webpack", ['clean', 'test-cover'], function(callback) {
     webpack(webpackConfig, function(err, stats) {
         if(err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString({
